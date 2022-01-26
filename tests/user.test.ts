7fdb250cd56edb15b55668c9d1a21685d44b8b25
pcsmomo <dongwoo_333@hanmail.net>
2022-01-26 18:58:46 +1100

    test: update test cases including checking token

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { app } from "../src/app";
-import { User } from "../src/models/user";
+import { User, UserDocument } from "../src/models/user";
 import { userOneId, userOne, setupDatabase } from "./fixtures/db";
 
 describe("User model and router", () => {
@@ -18,15 +18,22 @@ describe("User model and router", () => {
 
     // Assert that the database was changed correctly
     const user = await User.findById(response.body.user._id);
+
     expect(user).not.toBeNull();
+
+    // only for type guard
+    if (!user || (user && !user.tokens[0])) return;
 
     // Assert the response
     expect(user).toMatchObject({
-      name: "Noah",
-      email: "noah@example.com",
+      user: {
+        name: "Noah",
+        email: "noah@example.com",
+      },
+      token: user.tokens[0].token,
     });
 
-    expect(user!.password).not.toBe("pa123!@#");
+    expect(user.password).not.toBe("pa123!@#");
   });
 
   it("should sign in existing user", async () => {
@@ -37,5 +44,12 @@ describe("User model and router", () => {
         password: userOne.password,
       })
       .expect(200);
+
+    const user = await User.findById(userOneId);
+
+    // only for type guard
+    if (!user || (user && !user.tokens[1])) return;
+
+    expect(response.body.token).toBe(user.tokens[1].token);
   });
 });
